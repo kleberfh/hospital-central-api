c0aebc8d76944d50792e516d3d23b807aee12486
Kleber Fernando <canedokleberfilho@gmail.com>
2021-11-30 18:57:17 -0300

    arquivos da I.A e response do hospital

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import pickle
 
 app = Flask(__name__)
@@ -8,19 +8,48 @@ model = pickle.load(open(filename, 'rb'))
 
 
 @app.route('/')
-def predict_risk():  # put application's code here
+def api_status():  # put application's code here
     print('Model loaded')
     return "Api Ativa"
 
 
-@app.route('/api/get_user_risk', methods=['POST'])
-def get_user_risk():
+@app.route('/api/predict_risk', methods=['POST'])
+def predict_risk():
     content = request.json
     result = model.predict([[content['idade'], content['fumante'], content['obeso'], content['doenca_pulmonar'], content['nivel_sintomas']]])[0]
-    print(result)
     return {
         "risk": int(result)
     }
+
+
+@app.route('/api/hospitals', methods=['GET'])
+def get_hospitals():
+    return jsonify([
+        {
+            "id": 2,
+            "name": 'Hospital - ITE',
+            "coords": {
+                "latitude": -22.325118,
+                "longitude": -49.092425,
+            }
+        },
+        {
+            "id": 1,
+            "name": 'UPA Jardim América',
+            "coords": {
+                 "latitude": -22.344876,
+                 "longitude": -49.060491,
+            }
+        },
+        {
+            "id": 2,
+            "name": 'Unidade de saúde terra branca',
+            "coords": {
+                  "latitude": -22.348687,
+                  "longitude": -49.091489,
+            }
+        },
+    ])
 
 
 if __name__ == '__main__':
